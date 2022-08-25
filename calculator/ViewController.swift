a098eb3f71b1c086910530d9bb010ee2e226c1c9
HYORISIM <devyousim@gmail.com>
2022-08-25 14:24:14 +0900

    plus perform π

--- calculator/ViewController.swift
+++ calculator/ViewController.swift
@@ -8,12 +8,39 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    
+    @IBOutlet weak var display: UILabel!
+    
+    var userIsInTheMiddleOfTyping = false
+    
+    
+    @IBAction func touchDigit(_ sender: UIButton){
+        let digit = sender.currentTitle! //클릭한 버튼의 숫자 알기
+        print("wow \(digit) touched ")
+        if userIsInTheMiddleOfTyping
+        {
+            let textCurrentlyInDisplay = display.text! //현재 라벨에 붙어있는 숫자들
+            display.text = textCurrentlyInDisplay + digit //기존의 것 + 지금 누른 숫자 display에 넣기
+        }
+        else{
+            display.text = nil
+            display.text = digit //처음엔 무조건 비어지게
+        }
+        userIsInTheMiddleOfTyping = true
     }
-
+    
+    @IBAction func perform(_ sender: UIButton) {
+        userIsInTheMiddleOfTyping = false
+        if let methematicalSymbol = sender.currentTitle{
+            if methematicalSymbol == "π" {
+                display.text = String(M_PI)
+            }
+        }
+        
+    }
+    
+    
+    
 
 }
 
